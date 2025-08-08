989782d761ffb7356e5714f265b78694ed3671fc
Simon Ndege <simopetley@gmail.com>
2025-08-08 11:44:28 +0300

    add changes

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,11 @@
-#!/usr/bin/env python3
-
 class Person:
-    # Class body goes here
-
-    #Instance method definition
-    pass
+    def walk(self):
+        print("The person is walking.")  # Changed from "The person is walking!"
+    
+    def talk(self):
+        print("Hello World!")
+    
+    def introduce(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+        self.talk()
+        self.walk()
